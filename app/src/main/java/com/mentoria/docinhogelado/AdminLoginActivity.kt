d8ae4a72f2cbaed0c3645478aba5d7df9ce8471c
Lucas Ferreira <lucasfteo@gmail.com>
2022-11-16 18:41:20 -0300

    Criacao da activity admin

--- app/src/main/java/com/mentoria/docinhogelado/AdminLoginActivity.kt
+++ app/src/main/java/com/mentoria/docinhogelado/AdminLoginActivity.kt
@@ -2,14 +2,22 @@ package com.mentoria.docinhogelado
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mentoria.docinhogelado.database.AppDataBase
 import com.mentoria.docinhogelado.databinding.ActivityAdminLoginBinding
 
 class AdminLoginActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityAdminLoginBinding.inflate(layoutInflater)
     }
+    private val adminDao by lazy{
+        AppDataBase.instancia(this).adminDao()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        //Criar logica de cadastrar/autenticar admin
+        val btnLogin = binding.botaoEntrar
+        val btnCadastrar = binding.botaoCadastro
     }
 }
