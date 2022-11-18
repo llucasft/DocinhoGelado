20b193febb11be96738958c6bc75e7d8131d6372
Lucas Ferreira <lucasfteo@gmail.com>
2022-11-18 18:32:01 -0300

    Funcionalidades do ProdutoDao
    Adicionei dois metodos, um para inserir novos dados, e um para buscar os dados salvos no banco de dados.

--- app/src/main/java/com/mentoria/docinhogelado/database/dao/ProdutoDao.kt
+++ app/src/main/java/com/mentoria/docinhogelado/database/dao/ProdutoDao.kt
@@ -1,7 +1,17 @@
 package com.mentoria.docinhogelado.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.Query
+import com.mentoria.docinhogelado.model.Produto
 
 @Dao
 interface ProdutoDao {
+
+    @Insert(onConflict = REPLACE)
+    fun salva(vararg produto: Produto)
+
+    @Query("SELECT * FROM Produto")
+    fun buscaProdutos(): List<Produto>
 }
