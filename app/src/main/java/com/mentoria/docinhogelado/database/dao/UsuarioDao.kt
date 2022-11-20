0271058639b5a8434070adac7c578754761a2dc9
Lucas Ferreira <lucasfteo@gmail.com>
2022-11-20 16:01:46 -0300

    Criacao dos metodos de busca nos dao's

--- app/src/main/java/com/mentoria/docinhogelado/database/dao/UsuarioDao.kt
+++ app/src/main/java/com/mentoria/docinhogelado/database/dao/UsuarioDao.kt
@@ -1,7 +1,16 @@
 package com.mentoria.docinhogelado.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.mentoria.docinhogelado.model.Usuario
 
 @Dao
 interface UsuarioDao {
+
+    @Insert
+    fun salva(vararg usuario: Usuario)
+
+    @Query("SELECT * FROM Usuario")
+    fun buscaListaUsuario(): List<Usuario>
 }
