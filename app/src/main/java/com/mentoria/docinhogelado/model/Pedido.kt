60c7053a447734ade56ce869e693934aac46aa0b
Lucas Ferreira <lucasfteo@gmail.com>
2022-11-24 22:39:29 -0300

    Model Pedido
    Criei a model Pedido que ira conter os produtos contidos no pedido e o cliente que realizou o pedido.

---
+++ app/src/main/java/com/mentoria/docinhogelado/model/Pedido.kt
@@ -0,0 +1,10 @@
+package com.mentoria.docinhogelado.model
+
+import androidx.room.PrimaryKey
+
+data class Pedido(
+    @PrimaryKey
+    val id: Long = 0L,
+    val produtoId: String,
+    val usuarioId: String
+)
